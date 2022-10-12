805f5cb47df763352bab2fe3162dc88a5f570ab5
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-10-11 20:26:44 -0500

    feat: insertMany

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const estudiantes = [
     { nombre: 'José', apellido: 'Picos', edad: 29, dni: '34554398', curso: '2A', nota: 6 },
     { nombre: 'Lucas', apellido: 'Blanco', edad: 22, dni: '30355874', curso: '3A', nota: 10 },
     { nombre: 'María', apellido: 'García', edad: 36, dni: '29575148', curso: '1A', nota: 9 },
-    { nombre: 'Alex', apellido: 'Marin' },
+    // { nombre: 'Alex', apellido: 'Marin' },
     { nombre: 'Federico', apellido: 'Perez', edad: 41, dni: '320118321', curso: '2A', nota: 5 },
     { nombre: 'Tomas', apellido: 'Sierra', edad: 19, dni: '38654790', curso: '2B', nota: 4 },
     { nombre: 'Carlos', apellido: 'Fernández', edad: 33, dni: '26935670', curso: '3B', nota: 2 },
@@ -31,6 +31,8 @@ await mongoose.connect('mongodb://localhost/colegio', {
 
 console.log('Base de datos conectada')
 
-for (const estudiante of estudiantes) {
-    await EstudantesDAO.create(estudiante)
-}
+// for (const estudiante of estudiantes) {
+//     await EstudantesDAO.create(estudiante)
+// }
+
+await EstudantesDAO.insertMany(estudiantes) //bulk
